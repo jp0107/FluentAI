f7cd610be3f141e4ab28849e3a1ef133f8447f57
Jonathan Peixoto <jp5998@princeton.edu>
2024-04-10 17:42:02 -0400

    check for the prof-classes

--- fluentai.py
+++ fluentai.py
@@ -322,7 +322,7 @@ def add_course():
         return flask.jsonify({"message": "You are not allowed to create a course"}), 403
 
     # Add the course to the database
-    new_course = Course(course_id=course_id, course_name=course_name, course_description=course_description, language=language, created_at=datetime.fromisoformat(created_at))
+    new_course = Course(course_id=course_id, course_name=course_name, course_description=course_description, language=language, created_at=datetime.datetime.fromisoformat())
     with sqlalchemy.orm.Session(engine) as session:
         session.add(new_course)
         session.flush()  # Flush to get the new course ID if it's auto-generated
